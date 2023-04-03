96dbfceb37310f03e230bbfe99a4c5f953dc2f59
santicano <canosantiago404@gmail.com>
2023-04-03 19:15:23 -0300

    Revert "Arreglado errores"
    This reverts commit 8ab4f7536ca01b3a635174be2e9b1d9112d7dca0.

--- script.js
+++ script.js
@@ -39,7 +39,15 @@ backspace.addEventListener("click", function() {
 
 equal.addEventListener("click", function() {
 
-    checkCalculate();
+    if (currentValue != '' && previousValue != '') {
+        calculate();
+        screenPrevious.value = '';
+        if (previousValue.length <= 6) {
+            screenCurrent.value = previousValue;
+        } else {
+            screenCurrent.value = previousValue.slice(0, 6) + "...";
+        }
+    }
 
 });
 
@@ -52,22 +60,9 @@ function handleNumber(num) {
 }
 
 function handleOperator(op) {
-    checkCalculate();
     operator = op;
     previousValue = currentValue;
     currentValue = '';
-}
-
-function checkCalculate() {
-    if (currentValue != '' && previousValue != '') {
-        calculate();
-        screenPrevious.value = '';
-        if (previousValue.length <= 9) {
-            screenCurrent.value = previousValue;
-        } else {
-            screenCurrent.value = previousValue.slice(0, 9) + "...";
-        }
-    }
 }
 
 function calculate() {
