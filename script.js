df96d3384fb71f9a2e9ee2cc84cac9953f299859
santicano <canosantiago404@gmail.com>
2023-04-02 22:43:21 -0300

    Añadida la funcion operate solo con console.log

--- script.js
+++ script.js
@@ -40,3 +40,21 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case '+' :
+            return add(a, b);
+        case '-' :
+            return subtract(a, b);
+        case '*' :
+            return multiply(a, b);
+        case '/' :
+            if (b == 0) return null;
+            return divide(a, b);
+        default:
+            return null;
+    }
+}
