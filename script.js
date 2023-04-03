98264c760e667898caf0d74839f169375e3ca5e2
santicano <canosantiago404@gmail.com>
2023-04-03 17:29:29 -0300

    Rediseño

--- script.js
+++ script.js
@@ -5,18 +5,42 @@ const screen = document.querySelector('#result');
 let expression = '';
 let firstNumber;
 let secondNumber;
-let operator;
+let operator = '';
 
 /// FUNCIONES
 
 function insert(value) {
+    if (value == "+") {
+        firstNumber = expression;
+        expression = '';
+        operator = "+";
+    } else if (value == "-") {
+        firstNumber = expression;
+        expression = '';
+        operator = "-";
+    } else if (value == '*') {
+        firstNumber = expression;
+        expression = '';
+        operator = "*";
+    } else if (value == '/') {
+        firstNumber = expression;
+        expression = '';
+        operator = "/";
+    } 
+
     expression += value;
     screen.value = expression;
+    console.log(operator);
+    console.log(expression);
+
+
 }
 
 function clearScreen() {
     expression = '';
     screen.value = '';
+    firstNumber = "";
+    operator = "";
 }
 
 function del() {
@@ -26,19 +50,27 @@ function del() {
 
 
 function add(a, b) {
-    return a + b;
+    firstNumber = "";
+    expression = a + b;
+    screen.value = expression;
 }
 
 function subtract(a, b) {
-    return a - b;
+    firstNumber = "";
+    expression = a - b;
+    screen.value = expression;
 }
 
 function multiply(a, b) {
-    return a * b;
+    firstNumber = "";
+    expression = a * b;
+    screen.value = expression;
 }
 
 function divide(a, b) {
-    return a / b;
+    firstNumber = "";
+    expression = (a / b);
+    screen.value = expression;
 }
 
 function operate(operator, a, b) {
